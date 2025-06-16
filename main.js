919eb3669824a4e5a3c2a17a275c020a654d688d
Yulduz_Oromova <mahmurbekovna@gmail.com>
2025-06-16 22:32:49 +0500

    Projects section was completed

---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $('#imageModal').on('show.bs.modal', function (e) {
+    let imgSrc = $(e.relatedTarget).data('bs-img');
+    $('#modalImage').attr('src', imgSrc);
+  });
+});
